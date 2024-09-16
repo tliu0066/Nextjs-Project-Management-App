86f5825d64992585a46fd5ca30c4900957f94f4f
tq0066 <60831189+tliu0066@users.noreply.github.com>
2024-09-15 22:04:36 -0300

    9.15 update
    tailwing.config.ts, dashboardwrapper, sidebar, navbar

---
+++ client/src/app/(components)/Navbar/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link, Search, Settings } from "lucide-react";
+
+const Navbar = () => {
+  return (
+    <div className="flex items-center justify-between bg-white px-4 py-3 dark:bg-black">
+      {/* Search Bar */}
+      <div className="flex items-center gap-8">
+        <div className="relative flex h-min w-[200px]">
+          <Search className="left-{40px} absolute top-1/2 mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer dark:text-white"></Search>
+          <input
+            className="w-full rounded border-none bg-gray-100 p-2 pl-8 placeholder-gray-500 focus:border-transparent focus:outline-none dark:bg-gray-700 dark:text-white dark:placeholder-white"
+            type="search"
+            placeholder="Search..."
+          />
+        </div>
+      </div>
+
+        {/*Icons*/}
+        <div className="flex items-center">
+          <Link
+            href="/settings"
+            className="h-min w-min rounded p-2 hover:bg-green-100"
+          >
+            <Settings className="h-6 w-6 cursor-pointer dark:text-white" />
+          </Link>
+          <div className="ml-2 mr-5 hidden min-h-[2em] w-[0.1rem] bg-gray-200 md:inline-block"></div>
+        </div>
+      
+    </div>
+  );
+};
+
+export default Navbar;
