3073835500749001b795d0b6e723a5fc429c4422
tq0066 <60831189+tliu0066@users.noreply.github.com>
2024-09-17 11:29:48 -0300

    navbar,redux
    setup redux, add dark mode in navbar

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -1,11 +1,26 @@
 import React from "react";
-import { Link, Search, Settings } from "lucide-react";
+import { Link, Menu, Moon, Search, Settings, Sun } from "lucide-react";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
 
 const Navbar = () => {
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed,
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
   return (
     <div className="flex items-center justify-between bg-white px-4 py-3 dark:bg-black">
       {/* Search Bar */}
       <div className="flex items-center gap-8">
+        {!isSidebarCollapsed ? null : (
+          <button
+            onClick={() => dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))}
+          >
+            <Menu className="h-8 w-8 dark:text-white" />
+          </button>
+        )}
         <div className="relative flex h-min w-[200px]">
           <Search className="left-{40px} absolute top-1/2 mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer dark:text-white"></Search>
           <input
@@ -16,17 +31,34 @@ const Navbar = () => {
         </div>
       </div>
 
-        {/*Icons*/}
-        <div className="flex items-center">
-          <Link
-            href="/settings"
-            className="h-min w-min rounded p-2 hover:bg-green-100"
-          >
-            <Settings className="h-6 w-6 cursor-pointer dark:text-white" />
-          </Link>
-          <div className="ml-2 mr-5 hidden min-h-[2em] w-[0.1rem] bg-gray-200 md:inline-block"></div>
-        </div>
-      
+      {/*Icons*/}
+      <div className="flex items-center">
+        <button
+          onClick={() => dispatch(setIsDarkMode(!isDarkMode))}
+          className={
+            isDarkMode
+              ? `rounded p-2 dark:hover:bg-gray-700`
+              : `rounded p-2 hover:bg-gray-100`
+          }
+        >
+          {isDarkMode ? (
+            <Sun className="dark: h-6 w-6 cursor-pointer text-white" />
+          ) : (
+            <Moon className="h-6 w-6 cursor-pointer dark:text-white" />
+          )}
+        </button>
+        <Link
+          href="/settings"
+          className={
+            isDarkMode
+              ? `h-min w-min p-2 dark:hover:bg-gray-700`
+              : `h-min w-min p-2 hover:bg-gray-100`
+          }
+        >
+          <Settings className="h-6 w-6 cursor-pointer dark:text-white" />
+        </Link>
+        <div className="ml-2 mr-5 hidden min-h-[2em] w-[0.1rem] bg-gray-200 md:inline-block"></div>
+      </div>
     </div>
   );
 };
--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -39,4 +39,6 @@ const Sidebar = () => {
   );
 };
 
+
+
 export default Sidebar;
