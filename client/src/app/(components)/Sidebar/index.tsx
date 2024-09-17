eed48658e01be2b30bfbe3ffa0658cef4aee5404
tq0066 <60831189+tliu0066@users.noreply.github.com>
2024-09-17 10:08:27 -0300

    9.17
    add title and team name in navbar

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -1,13 +1,42 @@
-import React from 'react'
-
-
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import { LockIcon } from "lucide-react";
 
 const Sidebar = () => {
+  const [showProjects, setShowProjects] = useState(true);
+  const [showPriority, setShowPriority] = useState(true);
 
+  const sidebarClassNames = `fixed flex flex-col h-[100%] justify-between shadow-xl transition-all duration-300 h-full z-40 dark:bg-black over-flow-y-auto bg-white w-64 md:w-64`;
 
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div className={sidebarClassNames}>
+      <div className="flex h-[100%] w-full flex-col justify-start">
+        {/* TOP LOGO */}
+        <div className="z-50 flex min-h-[56px] w-64 items-center justify-between bg-white px-6 pt-3 dark:bg-black">
+          <div className="text-xl font-bold text-gray-800 dark:text-white">
+            EdDLIST
+          </div>
+        </div>
 
-export default Sidebar
+        {/** TEAM */}
+        <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
+          <Image src="/logo.png" alt="Logo" width={40} height={40}/>
+          <div>
+            <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
+              EDROH TEAM
+            </h3>
+            <div className="mt-1 flex items-start gap-2">
+              <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400"/>
+              <p className="text-xs text-gray-500">Private</p>
+            </div>
+          </div>
+        </div>
+
+        {/** NAVBAR LINKS */}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
