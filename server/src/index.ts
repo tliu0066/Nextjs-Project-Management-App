54fd1bb3a01dcbf44528cfce4402df2149d345f9
tq0066 <60831189+tliu0066@users.noreply.github.com>
2024-09-19 14:36:23 -0300

    set up database
    install prisma, add seed.data, setup prisma.schema

---
+++ server/src/index.ts
@@ -0,0 +1,7 @@
+import { Express } from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
